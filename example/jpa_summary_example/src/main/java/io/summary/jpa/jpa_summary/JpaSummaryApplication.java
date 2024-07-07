3a2372122a565d6a220f139c5492c8a4a35592f0
chagchagchag <chagchagchag.dev@gmail.com>
2024-07-07 23:37:51 +0900

    first commit

---
+++ example/jpa_summary_example/src/main/java/io/summary/jpa/jpa_summary/JpaSummaryApplication.java
@@ -0,0 +1,13 @@
+package io.summary.jpa.jpa_summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSummaryApplication.class, args);
+	}
+
+}
---
+++ example/jpa_summary_example/src/test/java/io/summary/jpa/jpa_summary/JpaSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.summary.jpa.jpa_summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
