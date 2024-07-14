24bbf9b41ff3a555ea296af30e0fd04e8fa0bcf0
chagchagchag <chagchagchag.dev@gmail.com>
2024-07-14 12:23:07 +0900

    chore : typo

--- example/jpa_summary_example/src/test/java/io/summary/jpa/jpa_summary/JpaSummaryApplicationTests.java
+++ example/jpa_summary_example/src/test/java/io/summary/jpa/jpa_summary/JpaSummaryApplicationTests.java
@@ -1,7 +1,5 @@
 package io.summary.jpa.jpa_summary;
 
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
