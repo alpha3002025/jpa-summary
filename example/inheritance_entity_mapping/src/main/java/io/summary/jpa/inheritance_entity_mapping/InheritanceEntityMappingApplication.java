4564fa0bf87cd4aeecb6051f4d0c694d2c6f0c6b
chagchagchag <chagchagchag.dev@gmail.com>
2024-07-15 15:50:57 +0900

    chore : project file (initial setting)

---
+++ example/inheritance_entity_mapping/src/main/java/io/summary/jpa/inheritance_entity_mapping/InheritanceEntityMappingApplication.java
@@ -0,0 +1,13 @@
+package io.summary.jpa.inheritance_entity_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritanceEntityMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InheritanceEntityMappingApplication.class, args);
+	}
+
+}
---
+++ example/inheritance_entity_mapping/src/test/java/io/summary/jpa/inheritance_entity_mapping/InheritanceEntityMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.summary.jpa.inheritance_entity_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InheritanceEntityMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
